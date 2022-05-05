eb566d6e7230b772dfd2859aaad3a3de200f8301
Wahyu Widyanto <44326845+yuuwid@users.noreply.github.com>
2022-05-05 23:05:07 +0700

    init
    version 0.0.1

---
+++ App/Controllers/ExampleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+use Lawana\Controller\BaseController,
+    Lawana\Utils\Request;
+
+class ExampleController extends BaseController
+{
+
+    public function index(Request $request)
+    {
+        return view('home.welcome-page');
+    }
+}
